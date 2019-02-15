37e54539d96fcb7cd7cd6457c293421d9353be99
Runeblaze <facss_carpool_service@unc.edu>
2019-02-14 19:31:00 -0500

    now ready to just do filtering

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+#![warn(clippy)]
 #[macro_use]
 extern crate lazy_static;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Result};
 use regex::Regex;
 use std::env;
+use std::collections::HashSet;
 
 const SPECIES : &str = "Mus musculus";
 
@@ -18,6 +20,19 @@ fn species_match(text : &str) -> Option<String> {
     }
 }
 
+fn name_match(text : &str) -> Option<String> {
+    lazy_static! {
+        static ref RE : Regex = Regex::new("NAME  ([[:alnum:]_]+)").unwrap();
+    }
+
+    if let Some(cap) = RE.captures(text) {
+        // println!("{:?}", cap[1].to_string());
+        Some(cap[1].to_string())
+    } else {
+        None
+    }
+}
+
 fn is_line_end(text : &str) -> bool {
     text.starts_with("//")
 }
@@ -25,17 +40,21 @@ fn is_line_end(text : &str) -> bool {
 // essentially tagged strings
 struct HmmEntry {
     raw : String,
-    species : Option<String>
+    species : Option<String>,
+    name : Option<String>,
 }
 
 impl HmmEntry {
     fn new() -> HmmEntry {
-        HmmEntry {raw: String::new(), species: None}
+        HmmEntry {raw: String::new(), species: None, name: None}
     }
 
     fn append(&mut self, line : &str) {
         if let Some(species) = species_match(line) {
             self.species = Some(species);
+        }
+        if let Some(name) = name_match(line) {
+            self.name = Some(name);
         }
         if !self.is_fit() {
             return;
@@ -56,22 +75,42 @@ impl HmmEntry {
 fn main() {
     let path = env::args().nth(1).unwrap();
     let file = File::open(path).unwrap();
+
+    let mut models = HashSet::new();
+    let model_names = vec![
+        String::from("ERVB7_1-LTR_MM"),
+        String::from("ERVB4_2-LTR_MM"),
+        String::from("RLTRETN_Mm"),
+        String::from("IAPLTR1_Mm"),
+        String::from("MT2_Mm"),
+        String::from("IAPEY3C_LTR"),
+        String::from("L1MdA_I_5end"),
+        String::from("L1MdTf_I_5end"),
+    ];
+
+    for n in model_names.iter() {
+        models.insert(n);
+    }
+    // models.insert(value: T)
     // let mut hmms : Vec<HmmEntry> = vec![];
     let mut cur_hmm = HmmEntry::new();
-    let mut cnt : u32 = 0;
+    // let mut cnt : u32 = 0;
     for maybe_line in BufReader::new(file).lines() {
         let line = maybe_line.unwrap();
         cur_hmm.append(&line);
         if is_line_end(&line) {
-            if cur_hmm.is_fit() {
-                println!("{}", cur_hmm.raw);
-                cnt += 1;
-                if cnt >= 500 {
-                    break;
+            // if cur_hmm.is_fit() {
+                if models.contains(&cur_hmm.name.unwrap()) {
+                    println!("{}", cur_hmm.raw);
                 }
-            }
+                
+                // cnt += 1;
+                // if cnt >= 500 {
+                //     break;
+                // }
+            // }
             cur_hmm = HmmEntry::new();
         }
     }
-    println!("cnt: {}", cnt);
+    // println!("cnt: {}", cnt);
 }
